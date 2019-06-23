c7ae73f8c30b6cff0cc0110ba56ec455853bfc4c
Svitlana Grytsai <svitlanagrytsai@Svitlanas-MacBook-Air.local>
2019-06-23 17:49:36 +0200

    neue komponente angelegt

--- mvp/src/app/app.module.ts
+++ mvp/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContentComponent } from './content/content.component';
+import { EventComponent } from './event/event.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ContentComponent,
+    EventComponent
   ],
   imports: [
     BrowserModule,
