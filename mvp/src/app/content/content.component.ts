17fc737e9d53b35d921a8f841ca04296f0c8601d
Svitlana Grytsai <svitlanagrytsai@Svitlanas-MacBook-Air.local>
2019-07-05 21:02:49 +0200

    events array sortiert

--- mvp/src/app/content/content.component.ts
+++ mvp/src/app/content/content.component.ts
@@ -7,24 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContentComponent implements OnInit {
 
-  events = [
-    {name: 'GOTO BERLIN 2019',
-     url: 'https://lorempixel.com/400/200/business/5',
-    homePageUrl: 'https://gotober.com',
-    startDatum: new Date('2019, 10, 23'),
-    endDatum: new Date('2019, 10, 25')},
-    {name: 'BED CON 2019',
-     url: 'https://lorempixel.com/400/200/business/1',
-     homePageUrl: 'http://bed-con.org',
-     startDatum: new Date('2019, 09, 05'),
-     endDatum: new Date('2019, 09, 06')},
-    
-  ]
-  
+  public events:any[]
+  public unsortetEvents:any[]
 
   constructor() { }
 
   ngOnInit() {
+  
+    this.unsortetEvents = [
+      {name: 'GOTO BERLIN 2019',
+       url: 'https://lorempixel.com/400/200/business/5',
+      homePageUrl: 'https://gotober.com',
+      startDatum: new Date('2019, 10, 23'),
+      endDatum: new Date('2019, 10, 25'),
+      todayDate: new Date()},
+      
+      {name: 'BED CON 2019',
+       url: 'https://lorempixel.com/400/200/business/1',
+       homePageUrl: 'http://bed-con.org',
+       startDatum: new Date('2019, 09, 05'),
+       endDatum: new Date('2019, 09, 06'),
+       todayDate: new Date()},
+      
+    ]
+
+    this.events = this.unsortetEvents.sort((a, b) => new Date(a.startDatum).getTime() - new Date(b.startDatum).getTime());
   }
 
 }
